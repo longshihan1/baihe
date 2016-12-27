661cce5453fd1b6ed50ef781a8ece3a425ce72c4
longshihan1 <577093937@qq.com>
2016-12-27 18:31:03 +0800

    添加xpath

--- baihe/items.py
+++ baihe/items.py
@@ -10,19 +10,41 @@ from scrapy import Field
 
 
 class BaiheItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
     mongodb_id = Field()
     name = Field()
-    baiheid = Field()
-   # avatar = Field()
-   # description = Field()
-   # age=Field()
-   # height=Field()
-   # weight=Field()
-   # education=Field()
-    lovetype=Field()
-    img=Field()
-   # address=Field()
-    #income=Field()
+    baiheid = Field()#id
+    avatar = Field()#头像
+    description = Field()#描述
+    age = Field()#年龄
+    height = Field()#身高
+    weight = Field()#体重
+    education = Field()#教育程度
+    lovetype = Field()#恋爱类型
+    img = Field()#图片
+    address = Field()#现居地
+    income = Field()#收入
+    marriage=Field()#婚姻
+    self_evaluation=Field()#自我评价
+    account=Field()#户口
+    occupation=Field()#职业
+    profession=Field()#专业
+    familysituation=Field()#家庭情况
+    parentalstatus=Field()#父母情况
+    motherwork=Field()#母亲工作
+    fatherwork=Field()#父亲工作
+    marriagetime=Field()#结婚时间
+    dating=Field()#约会方式
+    otherfancy=Field()#对方看中
+    cooking=Field()#厨艺状态
+
+    manage=Field()#对方年龄
+    manheight = Field()  # 对方身高
+    manedu = Field()  # 对方学历
+    manincome = Field()  # 对方收入
+    manaddress = Field()  # 对方所在地
+
+    hobbies=Field()#爱好
+    bodytype=Field()#体型
+
+
     pass
--- baihe/spiders/baihespider.py
+++ baihe/spiders/baihespider.py
@@ -24,9 +24,39 @@ class BaiHeSpider(scrapy.Spider):
     def parse_detail(self, response):
         baihe_item = BaiheItem()
         response_selector = HtmlXPathSelector(response)
-        baihe_item['baiheid'] = response_selector.select(u'//div[@class="womanData"]/div[@class="data"]/dl/dt[1]').extract()
-        baihe_item['img'] = response_selector.select(u'//div[@class="big_pic"]/ul/li').extract()
-        baihe_item['name'] = response_selector.select(
-            u'//div[@class="womanData"]/div[@class="inter"]/div[@class="name"]/span[1]').extract()
-        baihe_item['lovetype'] = response_selector.select(u'//div[@class="womanData"]/div[@class="data"]/dl/dt[3]').extract()
+        baiheid=response_selector.select(u'//div[contains(@class, "data")]/dl/dt[1]').extract()
+        baihe_item['baiheid'] =baiheid.split('：')[1]
+        baihe_item['img'] = response_selector.select(u'//div[contains(@class, "big_pic")]/ul/li[1]/a/img/@src').extract()
+        baihe_item['name'] = response_selector.select(u'//div[contains(@class, "name")]').extract()
+        lovetype=response_selector.select(u'//div[contains(@class, "data")]/dl/dt[2]').extract()
+        baihe_item['lovetype'] = lovetype.split(':')[1]
+
+        baihe_item['description'] = response_selector.select(u'//div[contains(@class,"intr")]').extract()
+        ahwe=response_selector.select(u'//div[contains(@class, "inter")]/p').extract()
+        baihe_item['age'] = ahwe.split('//')[0]
+        baihe_item['height'] = ahwe.split('//')[1]
+        baihe_item['weight'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[1]/dd[1]').extract()
+        baihe_item['education'] = ahwe.split('//')[2]
+        baihe_item['address'] = ahwe.split('//')[3]
+        baihe_item['income'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[1]/dd[0]').extract()
+        baihe_item['marriage'] = ahwe.split('//')[4]
+        baihe_item['self_evaluation'] = response_selector.select(u'//div[contains(@class, "inter label")]').extract()
+        baihe_item['account'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[2]/dd[1]').extract()
+        baihe_item['occupation'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[2]/dd[2]').extract()
+        baihe_item['profession'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[2]/dd[3]').extract()
+        baihe_item['familysituation'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[3]/dd[1]').extract()
+        baihe_item['parentalstatus'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[3]/dd[2]').extract()
+        baihe_item['motherwork'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[3]/dd[3]').extract()
+        baihe_item['fatherwork'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[3]/dd[4]').extract()
+        baihe_item['marriagetime'] = response_selector.select(u'//dl[contains(@class, "r")]/dd[1]').extract()
+        baihe_item['dating'] = response_selector.select(u'//dl[contains(@class, "r")]/dd[2]').extract()
+        baihe_item['otherfancy'] = response_selector.select(u'//dl[contains(@class, "r")]/dd[3]').extract()
+        baihe_item['cooking'] = response_selector.select(u'//dl[contains(@class, "r")]/dd[4]').extract()
+        baihe_item['manage'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[5]/dd[1]').extract()
+        baihe_item['manheight'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[5]/dd[2]').extract()
+        baihe_item['manedu'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[5]/dd[3]').extract()
+        baihe_item['manincome'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[5]/dd[4]').extract()
+        baihe_item['manaddress'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[6]/dd[1]').extract()
+        baihe_item['hobbies'] = response_selector.select(u'//div[contains(@class, "like")]/div[contains(@class, "cont")]').extract()
+        baihe_item['bodytype'] = response_selector.select(u'//div[contains(@class, "perData")]/dl[1]/dd[7]').extract()
         yield baihe_item
